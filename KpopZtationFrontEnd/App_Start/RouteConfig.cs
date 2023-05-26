2a7e0718d3a1c72d2a585e39e5a41ab166bc7005
Tegar Abdullah <tegar.abdullah@binus.edu>
2023-05-26 10:21:10 +0700

    feat: Add custom route

---
+++ KpopZtationFrontEnd/App_Start/RouteConfig.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNet.FriendlyUrls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Routing;
+
+namespace KpopZtationFrontEnd.App_Start
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            FriendlyUrlSettings settings = new FriendlyUrlSettings
+            {
+                AutoRedirectMode = RedirectMode.Permanent
+            };
+
+            routes.EnableFriendlyUrls(settings);
+            routes.MapPageRoute("home", "", "~/View/Home.aspx");
+            routes.MapPageRoute("login", "login", "~/View/Login.aspx");
+            routes.MapPageRoute("register", "register", "~/View/Register.aspx");
+            routes.MapPageRoute("profile", "profile", "~/View/Profile.aspx");
+            routes.MapPageRoute("transaction", "transaction", "~/View/Transaction.aspx");
+            routes.MapPageRoute("cart", "cart", "~/View/Carts.aspx");
+            routes.MapPageRoute("report", "report", "~/View/Reports.aspx");
+
+        }
+    }
+}
---
+++ KpopZtationFrontEnd/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="KpopZtationFrontEnd.Global" Language="C#" %>
